b409cad480c65dc555089171a012b7a2b7362e96
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-06-12 19:18:22 -0700

    New frontpage and footer.

--- homepage.php
+++ homepage.php
@@ -19,18 +19,18 @@ Template Name: Homepage
 
 <div class="container">
     
-    <section>
-        <div class="row section">             
+    <section class="highlevel-overview">
+        <div class="row">             
             <div class="col-lg-6">
                 <h3>What’s the issue?</h3>
                 <p>With so much information available online, trying to figure out which information is credible — and what is not — is a real challenge. When so much of what we read falls outside of our own expertise, how can we know which headlines and news articles are consistent with science?</p>
             </div>	
             <div class="col-lg-6">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/image1.jpg">
+                <img class="fig" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/image1.jpg">
             </div>
         </div>
 
-        <div class="row section">
+        <div class="row">
             <div class="col-lg-6">
                 <img alt="A daily mail article annotated by scientists." src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/image2.jpg" class="fig" />
                 <p class="caption"> Screenshot illustrating a possible use of the Hypothes.is annotator. Here a scientist added a comment and a link to a scientific publication.</p>
@@ -41,20 +41,20 @@ Template Name: Homepage
     	    </div>
        	</div>
 
-        <div class="row section">
+        <div class="row">
             <div class="col-lg-6">
                 <h3>Why should we care?</h3>
                 <p>An informed citizenry is essential for a functioning democracy. And to be properly informed, citizens need access to journalism of the highest accuracy and the tools to evaluate the credibility of what they read. This project aims to achieve both by 1) bringing the expertise of the scientific community into the world of online journalism, and 2) providing readers with top-level “credibility ratings” for a broad range of online news articles.</p>
             </div>
             <div class="col-lg-6">
-                <img alt="" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/image3.jpg">
+                <img alt="" class="fig" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/image3.jpg">
             </div>
         </div>
     </section>
     
     <hr/>
 
-    <section>
+    <section class="news-and-evaluations">
         <div class="row section">
           <div class="col-lg-6">
             <!-- News --> 
@@ -62,21 +62,21 @@ Template Name: Homepage
             <?php 
                 $args = array( 
                     'post_type' => array( 'post', 'press' ),
-                    'posts_per_page' => 5 );
+                    'posts_per_page' => 4 );
                 $loop = new WP_Query( $args );
             ?>
             <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
                 <article>
                     <header>
                     <?php if ( 'press' == get_post_type() ) : ?>
-                        <h3>
+                        <h4 class="headline">
                             <a href="<?php $key_1_value = get_post_meta( get_the_ID(), 'link', true );
                                 if( ! empty( $key_1_value ) ) { echo $key_1_value; } ?>"><?php the_title(); ?>
                             </a>
-                        </h3>
+                        </h4>
                     <?php endif; ?>
                     <?php if ( 'press' != get_post_type() ) : ?>
-                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <h4 class="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
                     <?php endif; ?>
                     </header>
                     <div class="media-left hidden-xs">
@@ -88,6 +88,7 @@ Template Name: Homepage
                         <?php the_excerpt(); ?>
                     </div>
                 </article>
+                <hr/>
             <?php endwhile; ?>
             <?php the_posts_navigation(); ?>
           </div>
@@ -98,21 +99,21 @@ Template Name: Homepage
             <?php 
                 $args = array( 
                     'post_type' => array( 'evaluation'),
-                    'posts_per_page' => 5 );
+                    'posts_per_page' => 4 );
                 $loop = new WP_Query( $args );
             ?>
             <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
                 <article>
                     <header>
                     <?php if ( 'press' == get_post_type() ) : ?>
-                        <h3>
+                        <h4 class="headline">
                             <a href="<?php $key_1_value = get_post_meta( get_the_ID(), 'link', true );
                                 if( ! empty( $key_1_value ) ) { echo $key_1_value; } ?>"><?php the_title(); ?>
                             </a>
-                        </h3>
+                        </h4>
                     <?php endif; ?>
                     <?php if ( 'press' != get_post_type() ) : ?>
-                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <h4 class="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
                     <?php endif; ?>
                     </header>
                     <div class="media-left hidden-xs">
@@ -123,7 +124,8 @@ Template Name: Homepage
                     <div class="media-body small">
                         <?php the_excerpt(); ?>
                     </div>
-                </article> 
+                </article>
+                <hr/>
             <?php endwhile; ?>
             <?php the_posts_navigation(); ?>
           </div>
--- templates/footer.php
+++ templates/footer.php
@@ -1,34 +1,33 @@
 <footer class="content-info">
   <div class="container">
 	<div class="row">
-	  	<hr/>
 	  	<h2>climate feedback is <a href="">a scientific experiment...</a></h2>
+	  	<hr/>
 	  	<div class="col-lg-3">
-	  		<h3>HOSTED BY</h3>
-	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/ucmerced.jpg">
-	  		<p>The Center for Climate Communication at the University of California, Merced</p>
+	  		<h4>HOSTED BY</h4>
+	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/ucmerced.jpg" class="sponser">
+	  		<p class="small">The Center for Climate Communication at the University of California, Merced</p>
 	  	</div>
 	  	<div class="col-lg-3">
-	  		<h3>SUPPORTED BY</h3>
-	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/citris.jpg">
-	  		<ul>
-	  			<li><a href="">The Center for Information Technology Research in the Interest of Society (CITRIS)</a></li>
-	  			<li><a href="">Data and Democracy seed grant #2015-313</a></li>
-	  			<li><a href="">Generous support from private donors. Make a tax-deductible contribution today</a>.</li>
+	  		<h4>SUPPORTED BY</h4>
+	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/citris.jpg" class="sponser" style="float:left; margin-right:1em;">
+	  		<p class="small"><a href="">The Center for Information Technology Research in the Interest of Society (CITRIS)</a></p>
+	  		<p class="small"><a href="">Data and Democracy seed grant #2015-313</a></p>
+	  		<p class="small"><a href="">Generous support from private donors. Make a tax-deductible contribution today</a>.</p>
 	  		</ul>
 	  	</div>
 	  	<div class="col-lg-3">
-	  		<h3>IN PARTNERSHIP WITH</h3>
-	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/csrrt.jpg">
-	  		<p>Climate Science Rapid Response Team (CSRRT)</p>
+	  		<h4>IN PARTNERSHIP WITH</h4>
+	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/csrrt.jpg" class="sponser">
+	  		<p class="small">Climate Science Rapid Response Team (CSRRT)</p>
 	  	</div>
 	  	<div class="col-lg-3">
-	  		<h3>LINKS</h3>
+	  		<h4>LINKS</h4>
 	  		<?php dynamic_sidebar('sidebar-footer'); ?>
 	    	<?php wp_nav_menu( array('container_class' => 'menu-footer', 'menu_class' => 'nav navbar-nav', 'theme_location' => 'secondary_navigation') ); ?>
-			<h3>CONTACT US</h3>
-			<p>Climate Feedback · Sierra Nevada Research Institute · 5200 N. Lake Road · Merced, CA 95343 · USA</p>
-			<p><a href="mailto:feedback@climatefeedback.org">feedback@climatefeedback.org</a></p>  	
+			<h4 style="margin-top:4em;">CONTACT US</h4>
+			<p class="small">Climate Feedback · Sierra Nevada Research Institute · 5200 N. Lake Road · Merced, CA 95343 · USA</p>
+			<p class="small"><a href="mailto:feedback@climatefeedback.org">feedback@climatefeedback.org</a></p>  	
 	  	</div>
   	</div>
   </div>
