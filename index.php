b7addbc39d45dac4a1fafc8bf5d5f0ee3a77a525
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-06-19 16:53:19 -0700

    Small update to footer style, removing code that doesn't belong in index.php.

--- index.php
+++ index.php
@@ -7,14 +7,6 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
-<?php 
-	$curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
-	$hypothesis = get_user_meta( $curauth->get('ID'), 'hypothesis', true );
-	$orchid = get_user_meta( $curauth->get('ID'), 'orchid', true );
-	print_r( $hypothesis );
-   	echo get_avatar( $curauth->get('ID'), $size = '96', $default = '<path_to_url>' );
-?>
-
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php endwhile; ?>
