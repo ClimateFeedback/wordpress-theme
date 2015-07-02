e5c51ed652bbe6ab2c1e514f936f4e5098ff66f7
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-07-02 14:37:18 -0700

    Styling updates.

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,15 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
-    <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">Climate Feedback</a>
+    </div>
+    <nav class="collapse navbar-collapse" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
