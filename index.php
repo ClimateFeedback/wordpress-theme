644bc690d796f3a4d1e05b7910bc83fc58c4a21b
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-07-05 15:29:43 -0700

    Press and news in main loop.

--- index.php
+++ index.php
@@ -7,8 +7,35 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+<?php
+    $args = array( 'post_type' => array( 'post', 'press' ));
+    $loop = new WP_Query( $args );
+?>
+
+<?php while ($loop->have_posts() ) : $loop->the_post(); ?>
+  <article>
+    <header>
+    <?php if ( 'press' == get_post_type() ) : ?>
+        <h4 class="headline">
+            <a href="<?php $key_1_value = get_post_meta( get_the_ID(), 'link', true );
+                if( ! empty( $key_1_value ) ) { echo $key_1_value; } ?>"><?php the_title(); ?>
+            </a>
+        </h4>
+    <?php endif; ?>
+    <?php if ( 'press' != get_post_type() ) : ?>
+        <h4 class="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+    <?php endif; ?>
+    </header>
+    <div class="media-left hidden-xs">
+        <a class="frontpagepostpic" href="<?php the_permalink(); ?>">
+          <?php the_post_thumbnail() ?>
+        </a>
+    </div>
+    <div class="media-body small">
+        <?php the_excerpt(); ?>
+    </div>
+	</article>
+	<hr/>
 <?php endwhile; ?>
 
 <?php the_posts_navigation(); ?>
