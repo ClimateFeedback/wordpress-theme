cdb48fddcd9ffb9591cdcc2d0925b8161b5141c5
Jake Hartnell <Jake.Hartnell@gmail.com>
2015-07-05 14:47:23 -0700

    styling updates.

--- templates/footer.php
+++ templates/footer.php
@@ -3,12 +3,12 @@
 	<div class="row">
 	  	<h3>climate feedback </h3>
 	  	<hr/>
-	  	<div class="col-sm-3">
+	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>HOSTED BY</h4>
 	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/ucmerced.jpg" class="sponser">
 	  		<p class="small">Center for Climate Communication, University of California, Merced</p>
 	  	</div>
-	  	<div class="col-sm-3">
+	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>SUPPORTED BY</h4>
 	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/citris.jpg" class="sponser" style="float:left; margin-right:1em;">
 	  		<p class="small">The Center for Information Technology Research in the Interest of Society (<a href="http://citris-uc.org/">CITRIS</a>)</p>
@@ -16,12 +16,12 @@
 	  		<p class="small">Generous support from private donors. <a href="contribute/">Make a tax-deductible contribution today</a>!</p>
 	  		</ul>
 	  	</div>
-	  	<div class="col-sm-3">
+	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>IN PARTNERSHIP WITH</h4>
 	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/csrrt.jpg" class="sponser">
 	  		<p class="small">Climate Science Rapid Response Team (<a href="http://www.climaterapidresponse.org/">CSRRT</a>)</p>
 	  	</div>
-	  	<div class="col-sm-3">
+	  	<div class="col-sm-3 col-xs-6">
 			<h4 >CONTACT US</h4>
 			<p class="small">Climate Feedback · Sierra Nevada Research Institute · 5200 N. Lake Road · Merced, CA 95343 · USA</p>
 			<p class="small"><a href="mailto:feedback@climatefeedback.org">feedback@climatefeedback.org</a></p>  	
