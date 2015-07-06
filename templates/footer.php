6bde4c2304fa871f64d1416576863962da671331
Manu <Manu@MacBook-Pro-de-Emmanuel.local>
2015-07-06 12:19:57 -0700

    front-page-style-update

--- templates/footer.php
+++ templates/footer.php
@@ -10,7 +10,7 @@
 	  	</div>
 	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>SUPPORTED BY</h4>
-	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/citris.jpg" class="sponser" style="float:left; margin-right:1em;">
+	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/citris-logo.png" class="sponser" style="float:left; margin-right:1em;">
 	  		<p class="small">The Center for Information Technology Research in the Interest of Society (<a href="http://citris-uc.org/">CITRIS</a>)</p>
 	  		<p class="small">Data and Democracy seed grant #2015-313</p>
 	  		<p class="small">Generous support from private donors. <a href="contribute/">Make a tax-deductible contribution today</a>!</p>
@@ -24,7 +24,13 @@
 	  	<div class="col-sm-3 col-xs-6">
 			<h4 >CONTACT US</h4>
 			<p class="small">Climate Feedback · Sierra Nevada Research Institute · 5200 N. Lake Road · Merced, CA 95343 · USA</p>
-			<p class="small"><a href="mailto:feedback@climatefeedback.org">feedback@climatefeedback.org</a></p>  	
+			<p class="small"><a href="mailto:feedback@climatefeedback.org">feedback@climatefeedback.org</a></p>  	      
+            <h4 class="spaceup1">FOLLOW US</h4>
+            <div class="social-footer">
+                <a href="https://www.facebook.com/pages/Climate-Feedback/1547593572151463" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="https://twitter.com/ClimateFdbk" target="_blank"><i class="fa fa-twitter"></i></a>
+                <a href="https://github.com/climatefeedback" target="_blank"><i class="fa fa-github"></i></a>
+            </div>
 	  	</div>
   	</div>
   </div>
