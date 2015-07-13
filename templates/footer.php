66cd52bb6244332df4d878b5e32663a7d81bed2a
Manu <Manu@MacBook-Pro-de-Emmanuel.local>
2015-07-13 12:22:57 -0700

    smaller-footer_scientists-call-bottom

--- templates/footer.php
+++ templates/footer.php
@@ -10,22 +10,22 @@
 	  	</div>
 	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>SUPPORTED BY</h4>
-	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/citris-logo.png" class="sponser" style="float:left; margin-right:1em;">
-	  		<p class="small">The Center for Information Technology Research in the Interest of Society (<a href="http://citris-uc.org/">CITRIS</a>)</p>
-	  		<p class="small">Data and Democracy seed grant #2015-313</p>
-	  		<p class="small">Generous support from private donors. <a href="contribute/">Make a tax-deductible contribution today</a>!</p>
+	  		<a target="_blank" href="http://citris-uc.org/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/citris-logo.png" class="sponser" style="float:left; margin-right:1em;"></a>
+	  		<p class="small">The Center for Information Technology Research in the Interest of Society:</p>
+            <p class="small"> seed grant Data and Democracy #2015-313</p>
+	  		<p class="small">Generous support from private donors. <a href="contribute/">Donate today</a>!</p>
 	  		</ul>
 	  	</div>
 	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>IN PARTNERSHIP WITH</h4>
-	  		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/csrrt.png" class="sponser">
-	  		<p class="small">Climate Science Rapid Response Team (<a href="http://www.climaterapidresponse.org/">CSRRT</a>)</p>
+	  		<a target="_blank" href="http://www.climaterapidresponse.org/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/csrrt.png" class="sponser"></a>
+	  		<p class="small">Climate Science Rapid Response Team </p>
 	  	</div>
 	  	<div class="col-sm-3 col-xs-6">
 			<h4 >CONTACT US</h4>
 			<p class="small">Climate Feedback · Sierra Nevada Research Institute · 5200 N. Lake Road · Merced, CA 95343 · USA</p>
 			<p class="small"><a href="mailto:feedback@climatefeedback.org">feedback@climatefeedback.org</a></p>  	      
-            <h4 class="spaceup1">FOLLOW US</h4>
+            <h4 class="spaceup05">FOLLOW US</h4>
             <div class="social-footer">
                 <a href="https://www.facebook.com/pages/Climate-Feedback/1547593572151463" target="_blank"><i class="fa fa-facebook"></i></a>
                 <a href="https://twitter.com/ClimateFdbk" target="_blank"><i class="fa fa-twitter"></i></a>
