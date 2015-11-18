0fc3554a6946a3ca1c415bb01355ff2c468d8e83
Vanessa Ceballos <Vanessa@Vanessas-MacBook-Pro.local>
2015-11-17 16:58:37 -0800

    fixed the head.php

--- templates/head.php
+++ templates/head.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
 <!--    twitter    -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@ClimateFdbk">
