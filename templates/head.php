056e95949b806a5b5ab4b855daa61e50a0b584bf
Manu <emvincent@nsup.org>
2016-05-10 16:45:47 -0700

    Fb pixel

--- templates/head.php
+++ templates/head.php
@@ -29,4 +29,21 @@
 		ga('create', 'UA-55053509-1', 'auto');
 		ga('send', 'pageview');
     </script>
+      <!-- Facebook Pixel Code -->
+      <script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '886662741444241');
+fbq('track', "PageView");
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=886662741444241&ev=PageView&noscript=1"
+/>
+      </noscript>
+      
+<!-- End Facebook Pixel Code -->
   </head>
