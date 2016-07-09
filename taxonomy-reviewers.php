6dc6b1582680196b976b62461d1f0d8d16b7e58e
Jake Hartnell <jake.hartnell@gmail.com>
2016-07-09 14:04:54 -0700

    Cleanup.

--- taxonomy-reviewers.php
+++ taxonomy-reviewers.php
@@ -15,7 +15,8 @@ $taxonomy = get_queried_object();
       </div>
         <!-- <h2>Articles reviewed by: <?php single_tag_title(); ?></h2>  -->
      
-        
+        <div class="row">
+          <div class="col-lg-12">
         <?php 
             $usrname = single_tag_title("", false);
             $rev = get_users( array( 'search' => $usrname  ) );
@@ -24,8 +25,7 @@ $taxonomy = get_queried_object();
               $usermeta = get_user_meta($usr->get('ID'));
                
               $output .= 
-                '<div class="row">
-                  <div class="media">
+                '<div class="media">
                     <div class="media-left">'
                       .get_avatar( $usr->get('ID'), $size = '256', $default = '<path_to_url>' )
                     .'</div>'
@@ -35,29 +35,34 @@ $taxonomy = get_queried_object();
                     .'</a></h2>'
 
                     .'<h3>'.esc_html( $usr->title ).', '.esc_html( $usr->affiliation ).'</h3>'
-               // .'</div></div>'
                     .'</div>'
 
-               // .'<div class="row">'
-               // .'<div class="col-lg-12>'
-               .'<h3>Details</h3>'
-               .'<p><a href="'.esc_html( $usr->user_url ).'">Website</a></p>'
-               .'<p>Hypothes.is handle: '.esc_html( $usermeta['hypothesis'][0] ).'</p>'
-               
-               .'<p><a href="'.esc_html( $usermeta['orcid'][0] ).'">Orcid</a></p>'
+                   .'<h3>Details</h3>'
+                   .'<p><a href="'.esc_html( $usr->user_url ).'">Website</a></p>'
+                   .'<p>Hypothes.is handle: '.esc_html( $usermeta['hypothesis'][0] ).'</p>'
+                   
+                   .'<p>
+                      <a href="'.esc_html( $usermeta['orcid'][0] ).'">Orcid</a>
+                    </p>'
 
-               .'<h3>Expertise</h3>'
-               .'<p>'.esc_html( $usermeta['expertise'][0] ).'</p>'
+                   .'<h3>Expertise</h3>'
+                   .'<p>'.esc_html( $usermeta['expertise'][0] ).'</p>'
 
-               .'<h3>Qualifying articles</h3>'
-               .'<p>'.esc_html( $usermeta['publicationone'][0] ).'</p>'
-               .'<p>'.esc_html( $usermeta['publicationtwo'][0] ).'</p>'
-               .'</div>'
+                   .'<h3>Qualifying articles</h3>'
+                   .'<p>'
+                    .esc_html( $usermeta['publicationone'][0] )
+                   .'</p>'
+                   .'<p>'.esc_html( $usermeta['publicationtwo'][0] ).'</p>'
+               // .'</div>'
+               // .'</div>'
                .'</div>';
               echo $output;
             } 
         ?>
-        
+          <h2>Article reviews</h2>
+          <hr/>
+        </div>
+        </div>
         
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
@@ -75,6 +80,7 @@ $taxonomy = get_queried_object();
 <?php while ($loop->have_posts() ) : $loop->the_post(); ?>
   
   <div class="row">
+    <div class="col-lg-12">
 
       <div class="media-left hidden-xs">
                 <a class="postpic" href=" <?php the_permalink(); ?>  ">
@@ -90,10 +96,9 @@ $taxonomy = get_queried_object();
                 <h4 class="noborder"><?php echo get_the_date( 'Y-m-d' ); ?></h4>
             <?php the_excerpt(); ?></p>
         </div>
+      </div>
     </div>
-	
-	<hr/>
-<?php endwhile; ?>
+	<?php endwhile; ?>
 
 <?php the_posts_navigation(); ?>
 
