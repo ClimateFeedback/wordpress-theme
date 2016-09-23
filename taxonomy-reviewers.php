3ff207ed27f87c4bc8b78819112ce3831e9a5617
Manu <emvincent@nsup.org>
2016-09-22 19:27:42 -0700

    t

--- taxonomy-reviewers.php
+++ taxonomy-reviewers.php
@@ -31,10 +31,12 @@ foreach ( $rev as $usr ) {
         
         
 <?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <!-- <?php get_search_form(); ?> -->
+  <!-- 
+    <div class="alert alert-warning">
+        <?php _e('Sorry, no results were found.', 'sage'); ?>
+    </div>
+  -->
+  <!-- <?php get_search_form(); ?> 
         
 <?php endif; ?>
 
