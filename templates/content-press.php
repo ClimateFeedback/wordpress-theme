31f56a9f98a7c7bfbc50e1d1d346f3de029a2651
Manu <emvincent@nsup.org>
2016-10-23 17:05:24 -0700

    press-post-style

--- templates/content-press.php
+++ templates/content-press.php
@@ -1,36 +1,13 @@
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-
+  <article <?php post_class(); ?> style="margin-top: 0px;">
     <header>
       <div class="page-header myfull">
           <h2  style="color:#000"> . </h2> 
       </div>
         <h1 class="entry-title"><?php the_title(); ?></h1> 
-        
-      
-      
-      <!--  <?php // get_template_part('templates/entry-meta'); ?>    --> 
-       <!-- <h4>-->
-      <!--        <a href="<?php // echo get_post_meta( get_the_ID(), 'link', true ); ?>" target="_blank">--> 
-      <!--          --><?php //echo get_post_meta( get_the_ID(), 'outlet', true ); ?>
-      <!--        </a>-->
-      <!--      </h4>-->
     </header>
 
     <div class="entry-content">
-    <!--      <button>
-      <div class="sharing-icons">
-        <?php if ( function_exists( 'sharing_display' ) ) {
-          sharing_display( '', true );
-        }
-
-        if ( class_exists( 'Jetpack_Likes' ) ) {
-          $custom_likes = new Jetpack_Likes;
-          echo $custom_likes->post_likes( '' );
-        } ?>
-      </div>
-
-    -->
         
       <p><em><?php the_excerpt(); ?></em> <a href="<?php echo get_post_meta( get_the_ID(), 'link', true) ?>" target="_blank">READ MORE</a></p>
       <center>
