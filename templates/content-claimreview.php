003479c89eb9326b6710cf54ea85915e9a202478
erwanledoux <lawanledoux@gmail.com>
2017-02-01 00:56:20 +0100

    made more responsive the claim review template

--- templates/content-claimreview.php
+++ templates/content-claimreview.php
@@ -8,36 +8,40 @@
     </header>
 
     <div class="entry-content">
-      <div class="fact-check-card">
-        <div class="fact-check-card__row row">
-          <div class="col-sm-3">
-            <a src=<?php echo get_post_meta( get_the_ID(), 'annotationsLink', true) ?>>
+      <div class="fact-check-card center">
+        <div class="fact-check-card__row row p2">
+          <div class="fact-check-card__row__screenshot col-sm-4 p1">
+            <a class="fact-check-card__row__screenshot__link"
+              href=<?php echo get_post_meta( get_the_ID(), 'annotationsLink', true) ?>
+            >
               <img
-                class="fact-check-card__row__screenshot"
+                class="fact-check-card__row__screenshot__img"
                 src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/<?php echo get_post_meta( get_the_ID(), 'screenshot', true)?>.png"
               >
             </a>
           </div>
-          <div class="col-sm-6">
-            <div class="fact-check-card__row__title"> CLAIM </div>
+          <div class="col-sm-4 p1">
+            <div class="fact-check-card-title mb2">
+              CLAIM
+            </div>
             <div>
               "<?php echo get_post_meta( get_the_ID(), 'claimshort', true) ?>"
             </div>
           </div>
-          <div>
-            <div class="fact-check-card__row__title">
+          <div class="col-sm-4 p1">
+            <div class="fact-check-card-title mb2">
               VERDICT
             </div>
             <div>
               <img
-                class="fact-check-card__row__verdict"
+                class="fact-check-card__row__verdict__img"
                 src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/<?php echo get_post_meta( get_the_ID(), 'verdict', true)?>.png"
               >
             </div>
           </div>
         </div>
-        <div class="fact-check-card__details">
-          <div class="fact-check-card__row__title">
+        <div class="mb3">
+          <div class="fact-check-card-title mb2">
             ISSUES
           </div>
           <div class="fact-check-card__details__text">
@@ -45,17 +49,17 @@
           </div>
         </div>
         <div>
-          <div class="fact-check-card__row__title">
+          <div class="fact-check-card-title mb2">
             KEY TAKE AWAY
           </div>
           <div class="fact-check-card__takeaway row">
-            <div class="fact-check-card__takeaway__icon col-sm-3">
+            <div class="fact-check-card__takeaway__icon col-sm-2 center">
               <img
                 class="fact-check-card__takeaway__icon__img"
                 src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/grey_bulb.png"
               >
             </div>
-            <div class="fact-check-card__takeaway__text col-sm-9">
+            <div class="fact-check-card__takeaway__text col-sm-10">
               <?php echo get_post_meta( get_the_ID(), 'takeaway', true) ?>
             </div>
           </div>
