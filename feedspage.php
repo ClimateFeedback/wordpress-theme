5e6600ec634f10232bce7a770d289ab821650454
erwanledoux <lawanledoux@gmail.com>
2017-02-28 21:11:38 +0100

    added sign up and cf logo

--- feedspage.php
+++ feedspage.php
@@ -17,6 +17,25 @@ Template Name: FeedsPage
   return $excerpt;
 }?>
 
+<div class="sigla feeds-hero">
+  <div class="container">
+    <div class="row feeds-hero__row">
+      <h3 class="tagline half-black">A Scientific Reference to Reliable Information on Climate Change</h3>
+    </div>
+  </div>
+</div>
+<div class="feeds-aim p3 overflow-hidden">
+  <img
+    class="feeds-aim__img col col-2 p2"
+    src="<?php echo get_site_url(); ?>/wp-content/uploads/2017/02/cf_photoicons_logo.png"
+  >
+  <p class="feeds-aim__text col col-10 p2">
+    We aim to help Internet users - from the general public to key policymakers -
+    distinguish inacuurate climate change narratives from scientifically sound
+    and trustworthy information web.
+  </p>
+</div>
+
 <div class="feeds-title h3">Latest Article Reviews</div>
 <?php
   $args = array(
@@ -87,6 +106,65 @@ Template Name: FeedsPage
   </a>
 </div>
 
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup" style="background-color: #F5F5F5;overflow-y:hidden;">
+  <form action="//climatefeedback.us9.list-manage.com/subscribe/post?u=e33d7323df2327db90438153a&amp;id=e4773425e1"
+    method="post" id="mc-embedded-subscribe-form"
+    name="mc-embedded-subscribe-form"
+    class="validate overflow-hidden"
+    target="_blank"
+    style="padding:5px;"
+  novalidate>
+    <div class="overflow-hidden p2">
+      <div class="feeds__sign-up__icon col col-md-2 p2">
+        <img
+          src="<?php echo get_site_url(); ?>/wp-content/uploads/2017/02/paper-plane-xxl.png"
+          style="width:60px;height:60px;"
+        >
+      </div>
+      <div class="col col-md-6 p2" style="color:#424242;line-height:1.3px;">
+        <label class="mt2 mb2" for="mce-EMAIL">Sign up for updates</label>
+        <p>
+          Help us create a better informed society. Join our community today!
+        </p>
+      </div>
+       <div
+        class="col col-md-4 feeds-inputs"
+        id="mc_embed_signup_scroll"
+      >
+         <div style="width:100%;">
+           <input type="email"
+            value=""
+            name="EMAIL"
+            class="email"
+            id="mce-EMAIL"
+            placeholder="enter your email address"
+            required
+            style="width:300px;"
+          >
+           <input type="submit"
+            value="Sign Up"
+            name="subscribe"
+            id="mc-embedded-subscribe"
+            class="button feeds-submit"
+            style="display:initial;width:300px;"
+          >
+         </div>
+         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+         <div style="position: absolute; left: -5000px;" aria-hidden="true">
+           <input type="text" name="b_e33d7323df2327db90438153a_e4773425e1" tabindex="-1" value="">
+        </div>
+       </div>
+     </div>
+  </form>
+</div>
+<!--End mc_embed_signup-->
 
 <div class="feeds-title h3">Latest Claim Reviews</div>
 <?php
