af3dab2511ec394269777d0e228a46a4c25fe1df
Manu <emvincent@nsup.org>
2017-05-19 10:29:57 -0700

    typ

--- templates/content-feedbacks.php
+++ templates/content-feedbacks.php
@@ -58,7 +58,7 @@
   <div class="bot-box">
       <p class="small spaceup1">
 <em>Climate Feedback is a non-partisan, non-profit organization dedicated to science education. Our reviews are crowdsourced directly from a community of scientists with relevant expertise. We strive to explain whether and why information is or is not consistent with the science and to help readers know which news to trust. <br />
-Please <a href="http://climatefeedback.org/process/#tit6">get in touch</a> if you have any comment or think there is an important claim or article that would need to be reviewed.</em>
+Please <a href="http://climatefeedback.org/contact-us/">get in touch</a> if you have any comment or think there is an important claim or article that would need to be reviewed.</em>
       </p>
   </div>
 
