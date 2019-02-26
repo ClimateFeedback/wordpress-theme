11e348f0d0d4b840ece15cb17c08d793b9de9a28
Manu <emvincent@nsup.org>
2019-02-26 14:19:06 +0100

    rss

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ $sage_includes = [
   'lib/assets.php',                // Scripts and stylesheets
   'lib/titles.php',                // Page titles
   'lib/extras.php',                // Custom functions
+  'lib/OpenGraph.php',             //Open graph
 ];
 
 foreach ($sage_includes as $file) {
