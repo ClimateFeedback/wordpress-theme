bf417e749e2b350e190b8ca6378a153e35b620dc
Manu <emvincent@nsup.org>
2019-03-13 17:51:48 +0100

    update link to standards

--- templates/footer.php
+++ templates/footer.php
@@ -12,7 +12,7 @@
 	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>COMMUNITY</h4>   
             <p class="small"><a href="/community/">Reviewers</a></p>
-            <p class="small"><a href="/community-standards/">Community standards</a></p>
+            <p class="small"><a href="http://sciencefeedback.co/community-standards/">Community standards</a></p>
             <p class="small"><a href="/for-scientists/">Apply to become a reviewer</a></p>
             <p class="small"><a href="/contact-us/">Submit an item to review</a></p>
 	  	</div>
