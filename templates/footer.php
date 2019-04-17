70bff4ca30b7d564b0b1066800e7a5d8b79217b7
Manu <emvincent@nsup.org>
2019-04-17 17:32:57 +0200

    https

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <?php if ( ! is_page_template( 'template-campaign.php' ) ) { ?>
-<footer class="content-info" role="contentinfo">
+<footer class="content-info" role="contentinfo" id="content-info">
   <div class="container">
 	<div class="row">
 	  	<h3>climate feedback </h3>
@@ -12,13 +12,13 @@
 	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>COMMUNITY</h4>   
             <p class="small"><a href="/community/">Reviewers</a></p>
-            <p class="small"><a href="http://sciencefeedback.co/community-standards/">Community standards</a></p>
+            <p class="small"><a href="https://sciencefeedback.co/community-standards/">Community standards</a></p>
             <p class="small"><a href="/for-scientists/">Apply to become a reviewer</a></p>
             <p class="small"><a href="/contact-us/">Submit an item to review</a></p>
 	  	</div>
 	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>ORGANIZATION</h4>
-            <p class="small"><a href="http://sciencefeedback.co/about/">About</a></p>
+            <p class="small"><a href="https://sciencefeedback.co/about/">About</a></p>
             <p class="small"><a href="/process/">Our method to evaluate articles</a></p>
             <p class="small"><a href="/claim-reviews-framework/">Our method to evaluate claims</a></p>
 	  	</div>
